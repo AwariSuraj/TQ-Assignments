0e1f0fe77a5c867ab7b731d21d3dc08e09a8104e
suraj <suraj@suraj.mshome.net>
2023-02-02 18:19:15 +0530

    BasicAssignments

---
+++ BasicVariablesDatatypes/src/com/Basicexercise/AreaCircle2.java
@@ -0,0 +1,12 @@
+package com.Basicexercise;
+
+public class AreaCircle2 {
+
+	public static void main(String[] args) {
+		double radius = 5;
+		double area = 0;
+		area = 3.14 * radius * radius;
+		System.out.println("area of circle is " + area);
+	}
+
+}
---
+++ BasicVariablesDatatypes/src/com/Basicexercise/Findsum1.java
@@ -0,0 +1,13 @@
+package com.Basicexercise;
+
+public class Findsum1 {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 10;
+		int sum = num1 + num2;
+		System.out.println("sum= " + sum);
+
+	}
+
+}
